1f6635a569eb52044d01eef23ddd8348b939ca4a
lucaslking <lucasgiovannidesigner@gmail.com>
2022-09-17 04:53:17 -0300

    Added timezone configuration

--- app/Controller/Admin/Settings.php
+++ app/Controller/Admin/Settings.php
@@ -10,8 +10,9 @@
 namespace App\Controller\Admin;
 
 use App\Utils\View;
-use App\Model\Functions\Server;
+use App\Model\Functions\Server as FunctionsServer;
 use App\Model\Entity\ServerConfig as EntityServerConfig;
+use DateTimeZone;
 
 class Settings extends Base{
     
@@ -75,13 +76,20 @@ class Settings extends Base{
                 return self::viewSettings($request, $status);
             }
 
+            if (empty($postVars['website_timezone'])) {
+                $status = Alert::getError('Defina o timezone.');
+                return self::viewSettings($request, $status);
+            }
+            $filter_timezone = filter_var($postVars['website_timezone'], FILTER_SANITIZE_SPECIAL_CHARS);
+
             EntityServerConfig::updateInfoWebsite('id = 1', [
                 'title' => $filter_title,
                 'downloads' => $filter_download,
                 'player_voc' => $filter_vocation,
                 'player_max' => $filter_maxplayers,
                 'player_guild' => $filter_levelguild,
-                'donates' => $filter_donates
+                'donates' => $filter_donates,
+                'timezone' => $filter_timezone
             ]);
             $status = Alert::getSuccess('Information successfully updated.');
             $sweetAlert = SweetAlert::Types('Success', 'Information successfully updated.', 'success', 'btn btn-success');
@@ -91,6 +99,7 @@ class Settings extends Base{
 
     public static function viewSettings($request, $status = null, $sweetAlert = null)
     {
+        $timeZones = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
         $dbServer = EntityServerConfig::getInfoWebsite()->fetchObject();
         $content = View::render('admin/modules/settings/index', [
             'status' => $status,
@@ -100,8 +109,10 @@ class Settings extends Base{
             'player_voc' => $dbServer->player_voc,
             'player_max' => $dbServer->player_max,
             'player_guild' => $dbServer->player_guild,
+            'webtimezone' => $dbServer->timezone,
+            'all_timezones' => $timeZones,
             'active_donates' => $dbServer->donates,
-            'worlds' => Server::getWorlds(),
+            'worlds' => FunctionsServer::getWorlds(),
         ]);
         return parent::getPanel('Settings', $content, 'settings');
     }
