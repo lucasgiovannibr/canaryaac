45f091e1ac384aeb94d7209b1a47684553b8c8e9
LIBERGOD <37589902+libergod@users.noreply.github.com>
2023-04-25 02:11:25 -0300

    fix: outfits env missing (#4)

--- app/Model/Functions/Player.php
+++ app/Model/Functions/Player.php
@@ -113,7 +113,7 @@ use App\Model\Functions\Guilds as FunctionsGuilds;
 
         public static function getOutfitImage($looktype = 0, $lookaddons = 0, $lookbody = 0, $lookfeet = 0, $lookhead = 0, $looklegs = 0, $mount = 0)
         {
-            $outfit = URL . '/outfit?id='.$looktype.'&addons='.$lookaddons.'&head='.$lookhead.'&body='.$lookbody.'&legs='.$looklegs.'&feet='.$lookfeet.'&mount='.$mount.'';
+            $outfit = URL . $_ENV['OUTFITS_FOLDER'] . '/outfit.php?id='.$looktype.'&addons='.$lookaddons.'&head='.$lookhead.'&body='.$lookbody.'&legs='.$looklegs.'&feet='.$lookfeet.'&mount='.$mount.'';
             return $outfit;
         }
 
